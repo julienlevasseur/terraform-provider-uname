a79d4456a53b0ad38215d7c8a50f38a156876215
Julien Levasseur <Julien Levasseur>
2023-03-08 16:48:10 -0500

    Rewrite provider

--- internal/provider/data_source_uname.go
+++ internal/provider/data_source_uname.go
@@ -1,71 +1,78 @@
 package provider
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/matishsiao/goInfo"
 )
 
-func dataSourceUname() *schema.Resource {
-	return &schema.Resource{
-		Read: dataSourceUnameRead,
+var (
+	_ datasource.DataSource = (*unameDataSource)(nil)
+)
 
-		Schema: map[string]*schema.Schema{
-			"kernel_name": {
-				Type:     schema.TypeString,
-				Computed: true,
+func NewUnameDataSource() datasource.DataSource {
+	return &unameDataSource{}
+}
+
+type unameDataSource struct{}
+
+func (u *unameDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Description: "Gets Uname informations from the system.",
+		Attributes: map[string]schema.Attribute{
+			"kernel_name": schema.StringAttribute{
+				Description: "kernel name",
+				Computed:    true,
 			},
-			"nodename": {
-				Type:     schema.TypeString,
-				Computed: true,
+			"node_name": schema.StringAttribute{
+				Description: "network node hostname",
+				Computed:    true,
 			},
-			"kernel_release": {
-				Type:     schema.TypeString,
-				Computed: true,
+			"kernel_release": schema.StringAttribute{
+				Description: "kernel release",
+				Computed:    true,
 			},
-			"machine": {
-				Type:     schema.TypeString,
-				Computed: true,
+			"machine": schema.StringAttribute{
+				Description: "machine hardware name",
+				Computed:    true,
 			},
-			"operating_system": {
-				Type:     schema.TypeString,
-				Computed: true,
+			"operating_system": schema.StringAttribute{
+				Description: "operating system",
+				Computed:    true,
 			},
 		},
 	}
 }
 
-func dataSourceUnameRead(d *schema.ResourceData, _ interface{}) error {
+func (n *unameDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_datasource"
+}
 
-	goInfo, err := goInfo.GetInfo()
-	if err != nil {
-		return err
+func (n *unameDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+
+	var config unameDataSourceModel
+	diags := req.Config.Get(ctx, &config)
+
+	goInfo, _ := goInfo.GetInfo()
+
+	state := unameDataSourceModel{
+		KernelName:      types.StringValue(goInfo.Kernel),
+		NodeName:        types.StringValue(goInfo.Hostname),
+		KernelRelease:   types.StringValue(goInfo.Core),
+		Machine:         types.StringValue(goInfo.Platform),
+		OperatingSystem: types.StringValue(goInfo.OS),
 	}
+	diags = resp.State.Set(ctx, state)
+	resp.Diagnostics.Append(diags...)
+}
 
-	err = d.Set("kernel_name", goInfo.Kernel)
-	if err != nil {
-		return err
-	}
-
-	err = d.Set("nodename", goInfo.Hostname)
-	if err != nil {
-		return err
-	}
-
-	err = d.Set("kernel_release", goInfo.Core)
-	if err != nil {
-		return err
-	}
-
-	err = d.Set("machine", goInfo.Platform)
-	if err != nil {
-		return err
-	}
-
-	err = d.Set("operating_system", goInfo.OS)
-	if err != nil {
-		return err
-	}
-
-	return nil
-
+type unameDataSourceModel struct {
+	KernelName      types.String `tfsdk:"kernel_name"`
+	NodeName        types.String `tfsdk:"node_name"`
+	KernelRelease   types.String `tfsdk:"kernel_release"`
+	Machine         types.String `tfsdk:"machine"`
+	OperatingSystem types.String `tfsdk:"operating_system"`
 }
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -1,15 +1,42 @@
 package provider
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/provider"
+	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
 )
 
-// Provider -
-func Provider() *schema.Provider {
-	return &schema.Provider{
-		Schema: map[string]*schema.Schema{},
-		DataSourcesMap: map[string]*schema.Resource{
-			"uname": dataSourceUname(),
-		},
+var (
+	_ provider.Provider = (*unameProvider)(nil)
+)
+
+type unameProvider struct{}
+
+func New() provider.Provider {
+	return &unameProvider{}
+}
+
+func (p *unameProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
+	resp.TypeName = "uname"
+}
+
+func (p *unameProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+
+}
+
+func (p *unameProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
+	return []func() datasource.DataSource{
+		NewUnameDataSource,
 	}
+}
+
+func (p *unameProvider) Resources(ctx context.Context) []func() resource.Resource {
+	return []func() resource.Resource{}
+}
+
+func (p *unameProvider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
+	resp.Schema = schema.Schema{}
 }
